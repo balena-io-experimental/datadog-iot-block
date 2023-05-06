ad4a79e620019bd14ab5a75deacb4271682f6c85
Ken Bannister <kb2ma@runbox.com>
2023-05-06 07:57:42 -0400

    Initial commit
    From https://github.com/balena-io-experimental/datadog-kerberos

---
+++ files/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z ${DD_API_KEY+x} ]
+then
+  echo "ERROR: DD_API_KEY IS NOT SET"
+  exit 1
+fi
+
+ln -sf /var/run/balena.sock /var/run/docker.sock
+
+echo "api_key: $DD_API_KEY" | cat - files/datadog.yaml > temp && mv temp files/datadog.yaml
+
+cat files/disk.yaml
+
+DD_HOSTNAME=`hostname` datadog-agent -c files/datadog.yaml run
